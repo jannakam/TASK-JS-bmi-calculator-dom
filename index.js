ec8f34d74fe4c63d4615ea6bee15bd5542e46112
jannakam <jannakalmuqaisib@gmail.com>
2024-10-07 11:39:49 +0300

    Fixed bmi calculation

--- index.js
+++ index.js
@@ -11,81 +11,65 @@ function calculateBMI() {
 
   // Challenge 1
   const heightInM = height * 0.01;
-  const bmi = (weight / (heightInM)^2);
+  const bmi = weight / (heightInM * heightInM);
   alert(bmi);
 
   // Challenge 2
   if (bmi < 18.5) {
     alert("Underweight");
-  }
-  else if (bmi > 18.5 && bmi < 24.9) {
-    alert("Normal");
-  }
-  else if (bmi > 25 && bmi < 29.9) {
+  } else if (bmi > 18.5 && bmi < 24.9) {
+    alert("Healthy Weight");
+  } else if (bmi > 25 && bmi < 29.9) {
     alert("Overweight");
-  }
-  else if (bmi > 30) {
+  } else if (bmi > 30) {
     alert("Obesity");
   }
 
   // Challenge 3
-  let healthy = false
+  let healthy = false;
   if (age > 19 && age < 24) {
     if (bmi > 19 && bmi < 24) {
       healthy = true;
-    }
-    else {
+    } else {
       healthy = false;
     }
-  }
-  else if (age > 25 && age < 34) {
+  } else if (age > 25 && age < 34) {
     if (bmi > 20 && bmi < 25) {
       healthy = true;
-    }
-    else {
+    } else {
       healthy = false;
     }
-  }
-  else if (age > 35 && age < 44) {
+  } else if (age > 35 && age < 44) {
     if (bmi > 21 && bmi < 26) {
       healthy = true;
-    }
-    else {
+    } else {
       healthy = false;
     }
-  }
-  else if (age > 45 && age < 54) {
+  } else if (age > 45 && age < 54) {
     if (bmi > 22 && bmi < 27) {
       healthy = true;
-    }
-    else {
+    } else {
       healthy = false;
     }
-  }
-  else if (age > 55 && age < 64) {
+  } else if (age > 55 && age < 64) {
     if (bmi > 23 && bmi < 28) {
       healthy = true;
-    }
-    else {
+    } else {
       healthy = false;
     }
-  }
-  else if (age > 65) {
+  } else if (age > 65) {
     if (bmi > 24 && bmi < 29) {
       healthy = true;
-    }
-    else {
+    } else {
       healthy = false;
     }
-  }
-  else {
+  } else {
     alert("Age is outside the valid range");
   }
 
   if (healthy === false) {
     alert("Unhealthy");
-  }
-  else {
+  } else {
     alert("Healthy");
   }
 }
