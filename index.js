987b5e87975c2e396d13a337f27dfa411b4c73ed
jannakam <jannakalmuqaisib@gmail.com>
2024-10-07 10:10:41 +0300

    bmi calculator web app with conditions

--- index.js
+++ index.js
@@ -8,4 +8,84 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+
+  // Challenge 1
+  const heightInM = height * 0.01;
+  const bmi = (weight / (heightInM)^2);
+  alert(bmi);
+
+  // Challenge 2
+  if (bmi < 18.5) {
+    alert("Underweight");
+  }
+  else if (bmi > 18.5 && bmi < 24.9) {
+    alert("Normal");
+  }
+  else if (bmi > 25 && bmi < 29.9) {
+    alert("Overweight");
+  }
+  else if (bmi > 30) {
+    alert("Obesity");
+  }
+
+  // Challenge 3
+  let healthy = false
+  if (age > 19 && age < 24) {
+    if (bmi > 19 && bmi < 24) {
+      healthy = true;
+    }
+    else {
+      healthy = false;
+    }
+  }
+  else if (age > 25 && age < 34) {
+    if (bmi > 20 && bmi < 25) {
+      healthy = true;
+    }
+    else {
+      healthy = false;
+    }
+  }
+  else if (age > 35 && age < 44) {
+    if (bmi > 21 && bmi < 26) {
+      healthy = true;
+    }
+    else {
+      healthy = false;
+    }
+  }
+  else if (age > 45 && age < 54) {
+    if (bmi > 22 && bmi < 27) {
+      healthy = true;
+    }
+    else {
+      healthy = false;
+    }
+  }
+  else if (age > 55 && age < 64) {
+    if (bmi > 23 && bmi < 28) {
+      healthy = true;
+    }
+    else {
+      healthy = false;
+    }
+  }
+  else if (age > 65) {
+    if (bmi > 24 && bmi < 29) {
+      healthy = true;
+    }
+    else {
+      healthy = false;
+    }
+  }
+  else {
+    alert("Age is outside the valid range");
+  }
+
+  if (healthy === false) {
+    alert("Unhealthy");
+  }
+  else {
+    alert("Healthy");
+  }
 }
